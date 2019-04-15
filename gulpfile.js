08f58ca5dcab42320115f682e72a04b84d1ff0b3
qisai <1193043457@qq.com>
2019-04-15 09:00:12 +0800

    222

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,20 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var webserver = require("gulp-webserver")
 var gulify = require("gulify");
+gulp.task("sass",function(){
+    return gulp.src('./src/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest("./src/sass/"))
+})
+gulp.task("webserver",function(){
+    return gulp.src('./src/')
+    .pipe(webserver({
+        port: 8080,
+        host: "localhost",
+        livereload: true
+    }))
+})
+gulp.task('watch', function() {
+    gulp.watch('./src/sass/*.scss', gulp.series('sass'))
+})
+gulp.task('dev', gulp.series("sass", "webserver", "watch"));
